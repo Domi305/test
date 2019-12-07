fc277b96e65ad2b2a802aa446e1c92445ac247e5
Dominik <dominik.stefanik@gmail.com>
2019-12-07 13:19:30 +0000

    kolejne dwie zmiany

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main (String[] args) {
         String mojString = "nowy String";
+        String mojString1 = "drugi String";
+        String mojString2 = "trzeci String";
         for (String s : args) {
             System.out.println (s);
             System.out.println ("hejka");
