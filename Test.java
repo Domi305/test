f6e95b85348ad0f96e8effe9c5ea6ce2c64616c6
Dominik <dominik.stefanik@gmail.com>
2019-12-07 11:42:18 +0000

    1st change

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main (String[] args) {
+        String mojString = "nowy String";
         for (String s : args) {
             System.out.println (s);
         }
